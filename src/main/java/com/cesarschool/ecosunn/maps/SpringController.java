2932a0db2e1ced239083ac1cfd6effdfc490b782
Emanuel Eduardo <112293589+couboii@users.noreply.github.com>
2024-11-19 22:30:32 -0300

    Adição do Pagamento

--- src/main/java/com/cesarschool/ecosunn/maps/SpringController.java
+++ src/main/java/com/cesarschool/ecosunn/maps/SpringController.java
@@ -153,5 +153,29 @@ public class SpringController {
     public String consultarTecnico(Model model) {
         return "consultatecnico"; // Nome do arquivo HTML na pasta "templates"
     }
+    // FAZER PEDIDO - DIRETÓRIO
+    @GetMapping("/fazerpedido")
+    public String fazerPedido(Model model) {
+        return "fazerpedido"; // Nome do arquivo HTML na pasta "templates"
+    }
+    // ADD PEDIDO - DIRETÓRIO
+    @GetMapping("/addpedido")
+    public String addPedido(Model model) {
+        return "addpedido"; // Nome do arquivo HTML na pasta "templates"
+    }
+    // REALIZAR PAGAMENTO - DIRETÓRIO
+    @GetMapping("/realizarPagamento")
+    public String realizarPagamento(Model model) {
+        return "realizarPagamento"; // Nome do arquivo HTML na pasta "templates"
+    }
+    // FAZER PAGAMENTO - DIRETÓRIO
+    @GetMapping("/fazerPagamento")
+    public String fazerPagamento(Model model) {
+        return "fazerPagamento"; // Nome do arquivo HTML na pasta "templates"
+    }
+
+
+
+
 
 }
